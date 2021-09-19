0c13afad3d7b1e371f8542698010e4637f002b14
devansgh <84060663+devansgh@users.noreply.github.com>
2021-09-19 14:33:48 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,13 @@
+const STATIC_ASSES = [
+    '/Menu.html'
+]
+
 self.addEventListener('install', event => {
-  console.log('V1 installing…');
-
-  // cache a cat SVG
-  event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('/cat.svg'))
-  );
-});
-
+    console.log("sw installed");
+})
 self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
-});
-
+    console.log("sw activate");
+})
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  // serve the cat SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/cat.svg'));
-  }
-});
+    console.log("sw fetch");
+})
